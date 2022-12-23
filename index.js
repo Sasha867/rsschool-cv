ac6dee284a5f02f7bdda78d4438ac3bab542b163
Sasha867 <sashacordoba@mail.ru>
2022-12-24 00:07:44 +0300

    feat: burger-menu and function toggler

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const burgerBar = document.getElementById("burgerBar");
+const nav = document.querySelector(".nav");
+const navList = document.querySelector(".nav-list");
+const navItems = document.querySelector(".nav-item");
 
+burgerBar.addEventListener("click", toggler);
+
+function toggler() {
+  burgerBar.classList.toggle("close-bar");
+  nav.classList.toggle("close-bar");
+  navList.classList.toggle("close-bar");
+}
